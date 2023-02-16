a932879fb004baea7341bd722b7f107a7e23cd9e
tincageorgianaalina <tincageorgianaalina@gmail.com>
2023-02-16 15:56:24 +0000

    review screenshots

--- src/main/java/alinatinca/utils/FileUtils.java
+++ src/main/java/alinatinca/utils/FileUtils.java
@@ -1,5 +1,6 @@
 package alinatinca.utils;
 
+import org.openqa.selenium.NoSuchSessionException;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,15 +9,26 @@ import java.io.IOException;
 
 public class FileUtils {
 
-    public static void takeScreenshot(ChromeDriver chromeDriver, String fileName){
-        File source = chromeDriver.getScreenshotAs(OutputType.FILE);
-        String destinatiionFile = System.getProperty("user.dir") + String.format("Logs/%s.png", fileName);
-        try{
-            org.apache.commons.io.FileUtils.copyFile(source, new File(destinatiionFile));
-        }
-        catch (IOException e){
-            System.out.print("The screenshot could not done!");
-            e.printStackTrace();
-        }
-    }
+//    public static void takeScreenshot(ChromeDriver chromeDriver, String fileName){
+//        File source = chromeDriver.getScreenshotAs(OutputType.FILE);
+////        String destinationFile = System.getProperty("user.dir")+ "C:\\\\Users\\\\Home\\\\IdeaProjects\""
+//        File destinationFile = new File(System.getProperty("user.dir") + "C:\\Users\\Home\\IdeaProjects");
+//        try{
+//            org.apache.commons.io.FileUtils.copyFile(source,destinationFile);
+//        }
+//        catch (Error | NoSuchSessionException, IOException e){
+//            System.out.print("The screenshot could not done!");
+//            e.printStackTrace();
+//        }
+//    }
+
+//    public static void takeScreenshot(ChromeDriver chromeDriver, String fileName){
+//        File screenshot = chromeDriver.getScreenshotAs(OutputType.FILE);
+//        try{
+//            org.apache.commons.io.FileUtils.copyFile(screenshot, new File("C:\\Users\\Home\\IdeaProjects\\Screenshots.png"));
+//        }
+//        catch (IOException e){
+//            System.out.print("The exception has been caught and the screenshot has been taken!");
+//        }
+//    }
 }
--- src/test/java/alinatinca/NotionsOfTheory/Screenshots.java
+++ src/test/java/alinatinca/NotionsOfTheory/Screenshots.java
@@ -1,9 +1,7 @@
 package alinatinca.NotionsOfTheory;
 
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
@@ -17,7 +15,7 @@ public class Screenshots extends BaseTest {
 
     @Test(description = "verify how to move to element and than click on it")
     public void actionsScrollTest()  {
-        ChromeDriver chromeDriver = null;
+
         try {
             chromeDriver.get("https://demoqa.com/");
             List<WebElement> card = chromeDriver.findElements(By.cssSelector(".card"));
@@ -27,6 +25,9 @@ public class Screenshots extends BaseTest {
             actions.moveToElement(sixthCard);
             //scroll on the page to the sixth card
             sixthCard.click();
+            File screenshot = chromeDriver.getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(screenshot, new File("C:\\Users\\Home\\IdeaProjects\\Screenshot.png"));
+
 
 //            File file = chromeDriver.getScreenshotAs(OutputType.FILE);
 //            File destFile = new File(System.getProperty("user.dir") + "\\src\\test\\java\\alinatinca\\NotionsOfTheory\\Screenshot1.png.png");
@@ -34,12 +35,25 @@ public class Screenshots extends BaseTest {
 //            FileUtils.copyFile(file,destFile);
 //            System.out.print("The screenshot has been created for this test case!");
 //            //take a screenshot
-
         } catch (Exception | Error e) {
             System.out.print("Exception has been caught!");
-
         } finally {
             quitChromeDriver();
         }
     }
+
+//    public static void main(String[] args) {
+////        //set the location of chrome browser
+////        System.setProperty("user.dir", "C:\\Users\\Home\\IdeaProjects")
+//        ChromeDriver chromeDriver = new ChromeDriver();
+////        chromeDriver.get("https://demoqa.com");
+//        File screenshot = chromeDriver.getScreenshotAs(OutputType.FILE);
+//        try{
+//            FileUtils.copyFile(screenshot, new File("C:\\Users\\Home\\IdeaProjects\\Photos.png"));
+//        }
+//        catch(IOException e){
+//            System.out.print(e.getMessage());
+//        }
+//        chromeDriver.close();
+//    }
 }
