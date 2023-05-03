df1b4f6e9fe7f805612948c2418ce2b21438dbdf
tincageorgianaalina <tincageorgianaalina@gmail.com>
2023-05-03 20:25:32 +0100

    variables part1

---
+++ src/main/java/alinatinca/HelloWorld.java
@@ -0,0 +1,16 @@
+package alinatinca;
+
+public class HelloWorld { //here the class starts
+    public static void main(String[] args) {
+        //from here Java starts to search the code
+        System.out.println("Hello World!");
+    }//the main function finished here
+}//the class finished here
+
+//comentariu on line
+
+/*
+comentariu multiple line
+line 2
+line 3
+ */
---
+++ src/main/java/alinatinca/Variables.java
@@ -0,0 +1,8 @@
+package alinatinca;
+
+public class Variables {
+    public static void main(String[] args) {
+        String brandCar = "Volvo";
+        String modelCar = "XC 60";
+    }
+}
