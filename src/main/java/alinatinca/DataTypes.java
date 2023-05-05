8b7107f87903ec395f860acd5c9011519e282a13
tincageorgianaalina <tincageorgianaalina@gmail.com>
2023-05-05 07:23:03 +0100

    variables part2 and data types

---
+++ src/main/java/alinatinca/DataTypes.java
@@ -0,0 +1,38 @@
+package alinatinca;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        /* The most useful 5 data types are:
+        String = " "
+         */
+        String brand = "Dacia";
+        String model = "1301";
+        String a = "3";
+        String b = "4";
+        System.out.println(a+b); //34
+        //write with upper letter
+        System.out.println(brand.toUpperCase());
+        //write with lower case
+        System.out.println(brand.toLowerCase());
+        //find the size
+        System.out.println(model.length());
+
+        //integer = whole number
+        int c = 3;
+        int d = 4;
+        System.out.println(c+d); //7
+
+        //double = decimal number
+        double price = 2300.50;
+
+        //boolean = true(1) or false(0)
+        boolean certified = false;
+
+        //char = a single character: #, &, $, d, Y, 2,..  ' '
+        char grade = 'A';
+
+
+
+
+    }
+}
--- src/main/java/alinatinca/Variables.java
+++ src/main/java/alinatinca/Variables.java
@@ -2,7 +2,30 @@ package alinatinca;
 
 public class Variables {
     public static void main(String[] args) {
+        //declaration and initialization:
         String brandCar = "Volvo";
         String modelCar = "XC 60";
+
+        //! strongly type (explicitly mode)- they are not recognized at runtime
+        System.out.println("I bought a car, brand: " + brandCar);
+        System.out.println("It is model: " + modelCar);
+
+        //overwrite:
+        System.out.println("After overwrite it will be: ");
+        brandCar = "XC 60 facelift";
+        System.out.println("It is model: " + brandCar);
+        System.out.println("It is model: " + modelCar);
+
+        //declaration:
+        String name;
+        String firstname;
+        int age;
+        //initialization:
+        name = "Patriche";
+        firstname = "Alina";
+        age = 29;
+        //concatenation:
+        System.out.println("My name is: " + name + " " + firstname + " and I am: " + age + " years old");
+        //automatically transform variables at runtime
     }
 }
